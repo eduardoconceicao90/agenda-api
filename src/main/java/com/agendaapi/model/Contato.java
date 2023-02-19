85cbd565202e6c660320f194e02a6b0d9ba09d7b
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-19 08:54:19 -0300

    Upload de arquivo com Spring Boot

--- src/main/java/com/agendaapi/model/Contato.java
+++ src/main/java/com/agendaapi/model/Contato.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,4 +33,8 @@ public class Contato implements Serializable {
 
     @Column
     private Boolean favorito;
+    
+    @Column
+    @Lob
+    private byte[] foto;
 }
